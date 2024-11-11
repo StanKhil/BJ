fa31a33ab82770fb4ac73436e599b5c1d8b40712
llstyle <weredhol@gmail.com>
2024-11-11 19:54:43 +0200

    wtf?

--- frontend/src/pages/Contest.vue
+++ frontend/src/pages/Contest.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
 import axios from 'axios';
 import Loader from '@/components/UI/Loader.vue';
 import router from '@/router';
@@ -7,18 +7,39 @@ import { useRoute } from 'vue-router';
 
 const route = useRoute();
 const loading = ref(true);
-const problems = ref([]);
+const contest = reactive({
+  id: '',
+  teamid: '',
+  problems: [],
+})
+const users = ref([])
+const problemsResult = ref([]);
 const getProblems = async () => {
   try {
     const response = await axios.get(`/contests/${route.params.id}`);
-    problems.value = response.data.problems;
+    contest.problems = response.data.problems;
+    contest.teamid = response.data.teamId;
+    contest.id = response.data.id;
+  } catch(e) {
+    console.log(e)
+  }
+}
+const getResult = async () => {
+  try {
+    users.value = (await axios.get(`/teams/${contest.teamid}`)).data.participants;
+    const response = await axios.get(`/contests/results/${route.params.id}`);
+    problemsResult.value = response.data;
   } catch(e) {
     console.log(e)
   } finally {
     loading.value = false;
   }
 }
-getProblems()
+onMounted(async () => {
+  await getProblems()
+  await getResult()
+})
+
 </script>
 
 <template>
@@ -28,12 +49,24 @@ getProblems()
     </div>
   </div>
   <div class="container" v-else>
-    <div>
-      <div class="problem-list">
-        <div v-for="problem in problems" class="problems"  @click="router.push(`/problem/${problem.id}`)">
-         <div class="problem-name">
-          {{problem.name}} ({{ problem.draft }}) ({{ problem.rating }})
-         </div>
+    <div class="table">
+      <div class="row">
+        <div class="td">+</div>
+        <div v-for="problem in problemsResult" @click="router.push(`/problem/${problem.id}`)" class="td">
+          {{ problem.name }}
+        </div>
+      </div>
+      <div v-for="user in users" class="row">
+        <div class="td">
+          {{ user.username }}
+        </div>
+        <div v-for="problem in problemsResult" class="td">
+          <div v-for="result in problem.submission" >
+            <div v-if="result.user.id === user.id">{{ result.verdict }}</div>
+          </div>
+          <div v-if="problem?.submission?.filter((s) => s.user.id === user.id).length === 0">
+            0
+          </div>
         </div>
       </div>
     </div>
@@ -76,5 +109,26 @@ getProblems()
   width: 100%;
   height: 100%;
   position: relative;
+}
+.table {
+  width: 100%;
+  height: 100%;
+  display: flex;
+
+}
+.row {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  
+  
+}
+.td {
+  cursor: pointer;
+  padding: 4px;
+  color: white;
+  background-color: #5083cf;
+  border: 1px solid white;
 }
 </style>
